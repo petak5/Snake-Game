8a44703e0095724eedf4298234a6e033e174777a
wavyhairdude <howdyder@outlook.com>
2017-04-20 15:46:44 -0400

    Update wallcollision.c

--- wallcollision.c
+++ wallcollision.c
@@ -1,1 +1,11 @@
 //check to see if the snake has hit the wall 
+int void wallcheck(posx,posy) {
+   if (--posx >= board_x || ++posx <= 0) {
+      return 12
+   }
+  else if (--posy >=board_y || ++posy <=0) {
+    return 12
+  }
+  else {
+    return 0
+  }
