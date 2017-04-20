df7275ef446d6fbfec3ceeae5dac9a6e2b33e3e4
mjkulesza <mateusz.jan.kulesza@gmail.com>
2017-04-20 20:18:15 +0200

    Update snake.c

--- snake.c
+++ snake.c
@@ -10,4 +10,34 @@ struct segment{
 	struct segment *next;
 };
 typedef struct segment segment;
-typedef segment* snake;
+typedef segment *snake;
+
+/* list manipulation */
+void prepend(snake sn, segment *sg)
+{
+	snake tmp = sn;
+	sn->next = sg;
+	sg->next = tmp;
+}
+
+void append(snake sn, segment *sg)
+{
+	snake tmp = sn;
+	while(tmp){
+		tmp = tmp->next;
+	}
+	tmp->next = sg;
+	sg->next = NULL;
+}
+
+/* returns 1 if snake intersects with itself */
+int intersect_snake(snake s)
+{
+	snake tmp = s;
+	while(tmp){
+		if(s->x == tmp->x && s->y == tmp->y)
+			return 1;
+		tmp = tmp->next;
+	}
+	return 0;
+}
