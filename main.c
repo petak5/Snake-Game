5679d3ab397d3ecf036ba238fa58865056c83d14
wavyhairdude <howdyder@outlook.com>
2017-04-19 22:30:10 -0400

    Update main.c

--- main.c
+++ main.c
@@ -13,4 +13,5 @@ void main() {
     }
    printf("#");
    printf("\n");
-  }
+  } //depending on OS, will need a clearscreen command ex: system("clear");
+// This makes the grid for the single snake block to be located. to move left right add/subtract from stopx same goes for stopy
