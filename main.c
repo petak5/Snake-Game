f94c0779a232c33be41a3e4256d24a6e09599214
wavyhairdude <howdyder@outlook.com>
2017-04-20 13:39:55 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-
+void move(int numerx,int numery);
 void main() {
+    int xs;
+    for (xs=0;xs<10;xs++) { 
+         move(xs,0);
+    }
+ 
+}
+
+void move(int numerx, int numery) {
   int x, y;
-  int stopx = 3;
-  int stopy = 2;
+  int stopx = 1+numerx;
+  int stopy = 1+numery;
   for (y = 0;y<stopy;y++) {
     
     printf("\n");
@@ -13,5 +21,9 @@ void main() {
     }
    printf("#");
    printf("\n");
+   system("sleep 1");
+   system("clear");
   } //depending on OS, will need a clearscreen command ex: system("clear");
 // This makes the grid for the single snake block to be located. to move left right add/subtract from stopx same goes for stopy
+
+    
