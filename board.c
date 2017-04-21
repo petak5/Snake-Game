fbce423d942118dcaf37d4f87fa8c35e3f7e17e0
sup3rn0va <Sup3rn0va@protonmail.com>
2017-04-21 19:02:57 +0100

    Quick draft
    still some errors and 80x40 is too big

--- board.c
+++ board.c
@@ -1,4 +1,52 @@
 
 //board for the game
+#include <stdio.h>
+
+
 #define BOARD_X  80
 #define BOARD_Y  40
+
+
+
+// Initializing the board
+int  board()
+{
+
+	
+	int x;
+	int y;
+
+	char  board[BOARD_Y][BOARD_X];
+	//looping through the board
+	for(int y = 0; y < BOARD_Y; y++) {
+		for(int x = 0; x < BOARD_X; x++) {
+			//even row
+			if((x + y) % 2 == 0)
+				board[y][x] = '|';
+			else
+				board[y][x] = '|';
+		}
+	}
+
+	//display the board
+	for(int y = 0; y < BOARD_Y; y++) {
+		for(int x = 0; x < BOARD_Y; x++) {
+			printf("%s",&board[y][x]);
+			printf("   ");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+int main()
+{
+
+
+	board();
+
+
+	return 0;
+}
+
