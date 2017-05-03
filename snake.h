7e0521a5bca2593c166a427ad5e088953ab6dfe6
mjkulesza <mateusz.jan.kulesza@gmail.com>
2017-05-03 17:27:08 +0200

    Update snake.h

--- snake.h
+++ snake.h
@@ -9,10 +9,13 @@ struct segment{
 typedef struct segment segment;
 typedef segment *snake;
 
+segment* create_segment(int x, int y);
 segment* prepend(snake sn, segment *sg);
 void append(snake sn, segment *sg);
+int append_xy(snake sn, int x, int y);
 segment* last_segment(snake s);
 int intersect_snake(snake s);
 void delete_segment(snake seg);
+void delete_last_segment(snake s);
 void move_snake(snake s, int move_x, int move_y);
 int check_snake(snake s, int x, int y);
