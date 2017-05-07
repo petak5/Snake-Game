bca5edc22cc40f6fe9cf9baefcfa82dac898ee2a
petak5 <peterurgos12345@gmail.com>
2017-05-07 19:26:30 +0200

    added board_prototype.c in which I separated the board() function into two separate functions init_board() and print_board()

---
+++ board_prototype.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define BOARD_X 80
+#define BOARD_Y 40
+
+void print_board(char board[][BOARD_X]);
+void init_board(char board[][BOARD_X]);
+
+int main(void)
+{
+	char  board[BOARD_Y][BOARD_X]; 
+	
+	init_board(board);
+	print_board(board);
+	return 0;
+}
+
+
+/* WORKING */
+void print_board(char board[][BOARD_X])
+{
+	int x;
+	int y;
+	for(int y = 0; y < BOARD_Y; y++) {
+		for(int x = 0; x < BOARD_X; x++) {
+			printf("%c",board[y][x]);
+		}
+	printf("\n");
+	}
+}
+
+/* WORKING */
+void init_board(char board[][BOARD_X])
+{
+	int x;
+	int y;
+
+	//looping through the board
+	for(int y = 0; y < (BOARD_Y); y++) {
+		for(int x = 0; x < (BOARD_X); x++) {
+			if ((x==0) || (x==(BOARD_X-1)))	
+						board[y][x] = '|';
+			else if ((y==0) || ((BOARD_Y-1)==y))
+				board[y][x] = '-';
+			else 
+				board[y][x] = ' ';
+		}
+	}
+}
