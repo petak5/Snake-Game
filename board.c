70056295b574c109254d3e7ed5dbb53d2a1e2ff5
petak5 <peterurgos12345@gmail.com>
2017-05-23 10:50:23 +0200

    merged board files

--- board.c
+++ board.c
@@ -1,48 +1,36 @@
-
-//board for the game
 #include <stdio.h>
+#include "board_prototype.h"
 
-
-#define BOARD_X  80
-#define BOARD_Y  40
-
-
-
-// Initializing the board
-int  board()
+/* prints an array */
+/* WORKING */
+void print_board(char board[][BOARD_X])
 {
+	int x;
+	int y;
+	for(int y = 0; y < BOARD_Y; y++) {
+		for(int x = 0; x < BOARD_X; x++) {
+			printf("%c",board[y][x]);
+		}
+	printf("\n");
+	}
+}
 
-	
+/* initializes array */
+/* WORKING */
+void init_board(char board[][BOARD_X])	/* it uses macro BOARD_X */
+{
 	int x;
 	int y;
 
-	char  board[BOARD_Y][BOARD_X]; 
 	//looping through the board
 	for(int y = 0; y < (BOARD_Y); y++) {
 		for(int x = 0; x < (BOARD_X); x++) {
-			//even row
 			if ((x==0) || (x==(BOARD_X-1)))	
 						board[y][x] = '|';
 			else if ((y==0) || ((BOARD_Y-1)==y))
 				board[y][x] = '-';
 			else 
-				board[y][x] = '0';
+				board[y][x] = ' ';
 		}
 	}
-
-	//display the board
-	for(int y = 0; y < BOARD_Y; y++) {
-		for(int x = 0; x < BOARD_X; x++) {
-			printf("%c",board[y][x]);
-		}
-		printf("\n");
-	}
-
-	return 0;
-}
-
-int main(void)
-{
-	board();
-	return 0;
 }
--- board_prototype.h
+++
@@ -1,5 +0,0 @@
-#define BOARD_X 40
-#define BOARD_Y 20
-
-void print_board(char board[][BOARD_X]);
-void init_board(char board[][BOARD_X]);
